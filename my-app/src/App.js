9cb37d61755dbe27ed97afa7e241b9bd83bb6182
Giang Tu <vantudie@gmail.com>
2019-02-16 19:34:43 +0700

    abc

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './page/hello.js';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Hello> </Hello>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
---
+++ my-app/src/pages/hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hello from '/page/hello.js';
+class Hello extends Component {
+  render() {
+    return (
+      <h1>Hello</h1>
+    );
+  }
+}
+
+export default Hello;
